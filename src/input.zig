c751b5a22b1f90d1cf6efca75ad597cb3f68716b
Jimmi Holst Chritsensen <jhc@dismail.de>
2019-08-07 21:31:03 +0200

    refactor input to work more like termbox

--- src/input.zig
+++ src/input.zig
@@ -4,206 +4,299 @@ const ascii = std.ascii;
 const debug = std.debug;
 const fs = std.fs;
 const math = std.math;
+const mem = std.mem;
+const unicode = std.unicode;
 
-// Key should really be a more close mapping to how the keys look that
-// we get from stdin. We want "Key.ctrl | 'h'" and "Key.ctrl | Key.backspace"
-// to actually conflict in a switch, because these are the same escape
-// sequence.
+// This was ported from termbox https://github.com/nsf/termbox (not complete)
+
 pub const Key = struct {
-    pub const Type = u32;
+    pub const Type = u64;
 
-    pub const unknown: Type = 0b11111111111111111111111111111111;
-    pub const esc: Type = '\x1b';
+    pub const unknown = 0xffffffffffffffff;
 
-    pub const enter: Type = ctrl | 'm';
-    pub const backspace: Type = ctrl | 'h';
+    pub const f1 = ctrl | (0xffff - 0);
+    pub const f2 = ctrl | (0xffff - 1);
+    pub const f3 = ctrl | (0xffff - 2);
+    pub const f4 = ctrl | (0xffff - 3);
+    pub const f5 = ctrl | (0xffff - 4);
+    pub const f6 = ctrl | (0xffff - 5);
+    pub const f7 = ctrl | (0xffff - 6);
+    pub const f8 = ctrl | (0xffff - 7);
+    pub const f9 = ctrl | (0xffff - 8);
+    pub const f10 = ctrl | (0xffff - 9);
+    pub const f11 = ctrl | (0xffff - 10);
+    pub const f12 = ctrl | (0xffff - 11);
 
-    pub const arrow_up: Type = 0x0100;
-    pub const arrow_down: Type = 0x0200;
-    pub const arrow_left: Type = 0x0300;
-    pub const arrow_right: Type = 0x0400;
-    pub const page_up: Type = 0x0500;
-    pub const page_down: Type = 0x0600;
-    pub const home: Type = 0x0700;
-    pub const end: Type = 0x0800;
-    pub const delete: Type = 0x0900;
-    pub const tab: Type = 0x0A00;
+    pub const insert = ctrl | (0xffff - 12);
+    pub const delete = ctrl | (0xffff - 13);
+    pub const home = ctrl | (0xffff - 14);
+    pub const end = ctrl | (0xffff - 15);
 
-    pub const ctrl: Type = 0b10000000000000000000000000000000;
-    pub const alt: Type = 0b01000000000000000000000000000000;
-    pub const shift: Type = 0b00100000000000000000000000000000;
+    pub const page_up = ctrl | (0xffff - 16);
+    pub const page_down = ctrl | (0xffff - 17);
+    pub const arrow_up = ctrl | (0xffff - 18);
+    pub const arrow_down = ctrl | (0xffff - 19);
+    pub const arrow_left = ctrl | (0xffff - 20);
+    pub const arrow_right = ctrl | (0xffff - 21);
 
-    pub fn toStr(key: Key.Type) []const u8 {
-        const key_no_modi = key & ~(ctrl | alt | shift);
-        switch (key & (ctrl | alt | shift)) {
-            ctrl => return toStrHelper(key_no_modi, "Ctrl+"),
-            alt => return toStrHelper(key_no_modi, "Shift+"),
-            shift => return toStrHelper(key_no_modi, "Ctrl+Alt+"),
-            ctrl | alt => return toStrHelper(key_no_modi, "Ctrl+Alt+"),
-            ctrl | shift => return toStrHelper(key_no_modi, "Ctrl+Shift+"),
-            alt | shift => return toStrHelper(key_no_modi, "Alt+Shift+"),
-            ctrl | alt | shift => return toStrHelper(key_no_modi, "Ctrl+Alt+Shift+"),
-            else => return toStrHelper(key_no_modi, ""),
-        }
-    }
+    pub const ctrl_arrow_up = ctrl | (0xffff - 22);
+    pub const ctrl_arrow_down = ctrl | (0xffff - 23);
+    pub const ctrl_arrow_left = ctrl | (0xffff - 24);
+    pub const ctrl_arrow_right = ctrl | (0xffff - 25);
 
-    // This will generate output a lot of strings to the binary. Is it ok?
-    // I should measure the actual binary cost of doing this "no failure toStr"
-    // function.
-    // Update: Building with release small + --strip:
-    // - toStr:    248K
-    // - no toStr: 160K
-    //
-    // Hmm, let's calculate.
-    // Printable chars: 95 * "Ctrl+".len + 95 * "Shift+".len + 95 * "Ctrl+Alt+".len
-    //                  + "Ctrl+Shift+".len + 95 * "Alt+Shift+".len + 95 * "Ctrl+Alt+Shift+".len
-    //                  + 95 = 4381
-    //
-    // I'm not gonna calc the rest (i think we hit around 8000 bytes). I'm not sure where the
-    // 80K comes from...
-    fn toStrHelper(key: Key.Type, comptime prefix: []const u8) []const u8 {
-        debug.assert(key & (ctrl | alt | shift) == 0);
-        switch (key) {
-            esc => return prefix ++ "Esc",
-            enter => return prefix ++ "Enter",
-            backspace => return prefix ++ "Backspace",
-            arrow_up => return prefix ++ "Up",
-            arrow_down => return prefix ++ "Down",
-            arrow_left => return prefix ++ "Left",
-            arrow_right => return prefix ++ "Right",
-            page_up => return prefix ++ "PageUp",
-            page_down => return prefix ++ "PageUp",
-            home => return prefix ++ "Home",
-            end => return prefix ++ "End",
-            delete => return prefix ++ "Delete",
-            ' ' => return prefix ++ "Space",
-            else => {
-                const small = math.cast(u8, key) catch 0;
-                if (!ascii.isPrint(small))
-                    return prefix ++ "???";
+    pub const shift_arrow_up = ctrl | (0xffff - 26);
+    pub const shift_arrow_down = ctrl | (0xffff - 27);
+    pub const shift_arrow_left = ctrl | (0xffff - 28);
+    pub const shift_arrow_right = ctrl | (0xffff - 29);
 
-                const printable = comptime blk: {
-                    var res: [('~' - ' ') + 1]u8 = undefined;
-                    for (res) |*c, i|
-                        c.* = @intCast(u8, i + ' ');
+    pub const ctrl_tilde = ctrl | 0x00;
+    pub const ctrl_2 = ctrl_tilde;
+    pub const ctrl_a = ctrl | 0x01;
+    pub const ctrl_b = ctrl | 0x02;
+    pub const ctrl_c = ctrl | 0x03;
+    pub const ctrl_d = ctrl | 0x04;
+    pub const ctrl_e = ctrl | 0x05;
+    pub const ctrl_f = ctrl | 0x06;
+    pub const ctrl_g = ctrl | 0x07;
+    pub const backspace = ctrl | 0x08;
+    pub const ctrl_h = backspace;
+    pub const tab = ctrl | 0x09;
+    pub const ctrl_i = tab;
+    pub const ctrl_j = ctrl | 0x0a;
+    pub const ctrl_k = ctrl | 0x0b;
+    pub const ctrl_l = ctrl | 0x0c;
+    pub const enter = ctrl | 0x0d;
+    pub const ctrl_m = enter;
+    pub const ctrl_n = ctrl | 0x0e;
+    pub const ctrl_o = ctrl | 0x0f;
+    pub const ctrl_p = ctrl | 0x10;
+    pub const ctrl_q = ctrl | 0x11;
+    pub const ctrl_r = ctrl | 0x12;
+    pub const ctrl_s = ctrl | 0x13;
+    pub const ctrl_t = ctrl | 0x14;
+    pub const ctrl_u = ctrl | 0x15;
+    pub const ctrl_v = ctrl | 0x16;
+    pub const ctrl_w = ctrl | 0x17;
+    pub const ctrl_x = ctrl | 0x18;
+    pub const ctrl_y = ctrl | 0x19;
+    pub const ctrl_z = ctrl | 0x1a;
+    pub const escape = ctrl | 0x1b;
+    pub const ctrl_lsq_bracket = escape;
+    pub const ctrl_3 = escape;
+    pub const ctrl_4 = ctrl | 0x1c;
+    pub const ctrl_backslash = ctrl_4;
+    pub const ctrl_5 = ctrl | 0x1d;
+    pub const ctrl_rsq_bracket = ctrl_5;
+    pub const ctrl_6 = ctrl | 0x1e;
+    pub const ctrl_7 = ctrl | 0x1f;
+    pub const ctrl_slash = ctrl_7;
+    pub const ctrl_underscore = ctrl_7;
+    pub const space = ctrl | 0x20;
+    pub const backspace2 = ctrl | 0x7f;
+    pub const ctrl_8 = backspace2;
 
-                    break :blk res;
-                };
-                inline for (printable) |c| {
-                    if (c == small)
-                        return prefix ++ [_]u8{c};
-                }
+    pub const alt = 1 << 63;
+    const ctrl = 1 << 62;
 
-                unreachable;
+    pub const Pick = enum {
+        NotCtrl, // picks non ctrl keys over ctrl keys
+        CtrlAlphaNum, // picks ctrl_a-z and ctrl_0-9 over other ctrl keys
+        CtrlSpecial1, // picks ctrl_{tidle, lsq/rsq_bracket, backslash, slash} over other ctrl keys
+        CtrlSpecial2, // picks ctrl_{tidle, lsq/rsq_bracket, backslash, undscore} over other ctrl keys
+    };
+
+    /// Returns null terminated array
+    pub fn toStr(key: Key.Type, pick: Pick) [32]u8 {
+        if (key == Key.unknown)
+            return "???\x00" ++ ([28]u8)(undefined);
+        const modi_str = switch (key & alt) {
+            alt => "alt+",
+            else => "",
+        };
+
+        var utf8_buf: [4]u8 = undefined;
+        const key_no_modi = key & ~Type(alt);
+        const key_str = switch (key_no_modi) {
+            f1 => "f1",
+            f2 => "f2",
+            f3 => "f3",
+            f4 => "f4",
+            f5 => "f5",
+            f6 => "f6",
+            f7 => "f7",
+            f8 => "f8",
+            f9 => "f9",
+            f10 => "f10",
+            f11 => "f11",
+            f12 => "f12",
+
+            insert => "insert",
+            delete => "delete",
+            home => "home",
+            end => "end",
+            backspace => switch (pick) {
+                .NotCtrl => "backspace",
+                else => "ctrl+h",
             },
-        }
+            tab => switch (pick) {
+                .NotCtrl => "tab",
+                else => "ctrl+i",
+            },
+            enter => switch (pick) {
+                .NotCtrl => "enter",
+                else => "ctrl+m",
+            },
+            escape => switch (pick) {
+                .NotCtrl => "escape",
+                .CtrlAlphaNum => "ctrl+3",
+                else => "ctrl+???", // TODO: ctrl_lsq_bracket
+            },
+            space => "space",
+            backspace2 => switch (pick) {
+                .NotCtrl => "backspace2",
+                else => "ctrl+8",
+            },
+
+            page_up => "page_up",
+            page_down => "page_down",
+            arrow_up => "arrow_up",
+            arrow_down => "arrow_down",
+            arrow_left => "arrow_left",
+            arrow_right => "arrow_right",
+
+            ctrl_arrow_up => "ctrl+arrow_up",
+            ctrl_arrow_down => "ctrl+arrow_down",
+            ctrl_arrow_left => "ctrl+arrow_left",
+            ctrl_arrow_right => "ctrl+arrow_right",
+
+            shift_arrow_up => "shift+arrow_up",
+            shift_arrow_down => "shift+arrow_down",
+            shift_arrow_left => "shift+arrow_left",
+            shift_arrow_right => "shift+arrow_right",
+
+            ctrl_a => "ctrl+a",
+            ctrl_b => "ctrl+b",
+            ctrl_c => "ctrl+c",
+            ctrl_d => "ctrl+d",
+            ctrl_e => "ctrl+e",
+            ctrl_f => "ctrl+f",
+            ctrl_g => "ctrl+g",
+            ctrl_j => "ctrl+j",
+            ctrl_k => "ctrl+k",
+            ctrl_l => "ctrl+l",
+            ctrl_n => "ctrl+n",
+            ctrl_o => "ctrl+o",
+            ctrl_p => "ctrl+p",
+            ctrl_q => "ctrl+q",
+            ctrl_r => "ctrl+r",
+            ctrl_s => "ctrl+s",
+            ctrl_t => "ctrl+t",
+            ctrl_u => "ctrl+u",
+            ctrl_v => "ctrl+v",
+            ctrl_w => "ctrl+w",
+            ctrl_x => "ctrl+x",
+            ctrl_y => "ctrl+y",
+            ctrl_z => "ctrl+z",
+
+            ctrl_2 => switch (pick) {
+                .NotCtrl, .CtrlAlphaNum => "ctrl+2",
+                else => "ctrl+~",
+            },
+
+            ctrl_4 => switch (pick) {
+                .NotCtrl, .CtrlAlphaNum => "ctrl+4",
+                else => "ctrl+\\",
+            },
+            ctrl_5 => switch (pick) {
+                .NotCtrl, .CtrlAlphaNum => "ctrl+5",
+                else => "ctrl+???", // TODO: ctrl_rsq_bracket
+            },
+            ctrl_6 => "ctrl+6",
+            ctrl_7 => switch (pick) {
+                .NotCtrl, .CtrlAlphaNum => "ctrl+7",
+                .CtrlSpecial1 => "ctrl+/",
+                .CtrlSpecial2 => "ctrl+_",
+            },
+            else => blk: {
+                if ((key & 0xffffffff00000000) != 0)
+                    break :blk "???";
+                const codepoint = @intCast(u32, key & 0x00000000ffffffff);
+                const len = unicode.utf8Encode(codepoint, &utf8_buf) catch unreachable;
+                break :blk ([]const u8)(utf8_buf[0..len]);
+            },
+        };
+
+        var res: [32]u8 = undefined;
+        mem.copy(u8, res[0..], modi_str);
+        mem.copy(u8, res[modi_str.len..], key_str);
+        mem.copy(u8, res[modi_str.len + key_str.len ..], "\x00");
+        return res;
     }
 };
+
+// TODO: this is not the exact escape keys for my terminal
+const esc_keys = [_][]const u8{
+    "\x1bOP",
+    "\x1bOQ",
+    "\x1bOR",
+    "\x1bOS",
+    "\x1b[15~",
+    "\x1b[17~",
+    "\x1b[18~",
+    "\x1b[19~",
+    "\x1b[20~",
+    "\x1b[21~",
+    "\x1b[23~",
+    "\x1b[24~",
+    "\x1b[2~",
+    "\x1b[3~",
+    "\x1b[H",
+    "\x1b[F",
+    "\x1b[5~",
+    "\x1b[6~",
+    "\x1b[A",
+    "\x1b[B",
+    "\x1b[D",
+    "\x1b[C",
+    "\x1b[1;5A",
+    "\x1b[1;5B",
+    "\x1b[1;5D",
+    "\x1b[1;5C",
+    "\x1b[1;2A",
+    "\x1b[1;2B",
+    "\x1b[1;2D",
+    "\x1b[1;2C",
+};
+
+fn parseKey(key: []const u8) Key.Type {
+    if (key.len == 1 and key[0] == '\x1b')
+        return Key.escape;
+    if (key[0] == '\x1b') {
+        for (esc_keys) |esc_key, i| {
+            if (mem.startsWith(u8, key, esc_key))
+                return Key.f1 - i;
+        }
+
+        return Key.alt | parseKey(key[1..]);
+    }
+
+    if (key[0] <= (Key.space & ~Key.Type(Key.ctrl)))
+        return Key.ctrl | Key.Type(key[0]);
+    if (key[0] == (Key.backspace2 & ~Key.Type(Key.ctrl)))
+        return Key.ctrl | Key.Type(key[0]);
+
+    const len = unicode.utf8ByteSequenceLength(key[0]) catch return Key.unknown;
+    if (key.len <= len)
+        return unicode.utf8Decode(key[0..len]) catch return Key.unknown;
+
+    return Key.unknown;
+}
 
 pub fn readKey(stdin: fs.File) !Key.Type {
     var buf: [10]u8 = undefined;
     var len: usize = 0;
     while (len == 0)
         len = try stdin.read(&buf);
-    const key = buf[0..len];
 
-    //debug.warn("------------\n");
-    //for (key) |b| {
-    //    if (ascii.isPrint(b)) {
-    //        debug.warn("{} 0x{x:2} {b:8} '{c}'\n", b, b, b, b);
-    //    } else {
-    //        debug.warn("{} 0x{x:2} {b:8}\n", b, b, b);
-    //    }
-    //}
-    switch (key.len) {
-        1 => switch (key[0]) {
-            '\x09' => return Key.tab,
-            '\x1b' => return Key.esc,
-            '\x7f' => return Key.backspace,
-            else => {
-                if (ascii.isPrint(key[0]))
-                    return key[0];
-                if (ascii.isPrint(key[0] | 0b01100000))
-                    return Key.ctrl | (key[0] | 0b01100000);
-
-                return Key.unknown;
-            },
-        },
-        2 => {
-            if (key[0] != '\x1b')
-                return Key.unknown;
-
-            if (ascii.isPrint(key[1]))
-                return Key.alt | key[1];
-            if (ascii.isPrint(key[1] | 0b01100000))
-                return Key.alt | Key.ctrl | (key[1] | 0b01100000);
-        },
-        3 => {
-            if (key[0] != '\x1b')
-                return Key.unknown;
-            switch (key[1]) {
-                '[' => switch (key[2]) {
-                    'A' => return Key.arrow_up,
-                    'B' => return Key.arrow_down,
-                    'C' => return Key.arrow_right,
-                    'D' => return Key.arrow_left,
-                    'H' => return Key.home,
-                    'F' => return Key.end,
-                    else => {},
-                },
-                'O' => switch (key[2]) {
-                    'H' => return Key.home,
-                    'F' => return Key.end,
-                    else => {},
-                },
-                else => {},
-            }
-        },
-        4 => {
-            if (key[0] != '\x1b')
-                return Key.unknown;
-            if (key[1] != '[')
-                return Key.unknown;
-
-            switch (key[3]) {
-                '~' => switch (key[2]) {
-                    '1' => return Key.home,
-                    '3' => return Key.delete,
-                    '4' => return Key.end,
-                    '5' => return Key.page_up,
-                    '6' => return Key.page_down,
-                    '7' => return Key.home,
-                    '8' => return Key.end,
-                    else => {},
-                },
-                else => {},
-            }
-        },
-        6 => {
-            if (key[0] != '\x1b')
-                return Key.unknown;
-            if (key[1] != '[')
-                return Key.unknown;
-            if (key[2] != '1')
-                return Key.unknown;
-            if (key[3] != ';')
-                return Key.unknown;
-            const modifier = switch (key[4]) {
-                '2' => Key.shift,
-                '5' => Key.ctrl,
-                else => return Key.unknown,
-            };
-
-            switch (key[5]) {
-                'A' => return modifier | Key.arrow_up,
-                'B' => return modifier | Key.arrow_down,
-                'C' => return modifier | Key.arrow_right,
-                'D' => return modifier | Key.arrow_left,
-                else => {},
-            }
-        },
-        else => {},
-    }
-
-    return Key.unknown;
+    return parseKey(buf[0..len]);
 }
--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,7 @@ const fs = std.fs;
 const heap = std.heap;
 const io = std.io;
 const math = std.math;
+const mem = std.mem;
 const process = std.process;
 
 const Editor = core.Editor;
@@ -44,6 +45,7 @@ const file_name_view = draw.stack(.Horizontal, struct {
 const info_view = draw.right(draw.value("", struct {
     hint: []const u8 = default_hint,
     location: core.Location = core.Location{},
+    key: Key.Type = Key.space,
     allocated_bytes: usize = 0,
     freed_bytes: usize = 0,
     text_size: usize = 0,
@@ -60,11 +62,12 @@ const info_view = draw.right(draw.value("", struct {
             context,
             Errors,
             output,
-            "{}{} ({},{}) (t:{B:2} a:{B:2} f:{B:2})",
+            "{}{} ({},{}) ('{s}') (t:{B:2} a:{B:2} f:{B:2})",
             if (self.hint.len != 0) " " else "",
             self.hint,
             self.location.line + 1,
             self.location.column + 1,
+            ([*]const u8)(&Key.toStr(self.key, .NotCtrl)),
             self.text_size,
             self.allocated_bytes,
             self.freed_bytes,
@@ -95,33 +98,33 @@ const help_popup = blk: {
     @setEvalBranchQuota(10000);
     break :blk draw.visible(.Hide, draw.center(draw.clear(draw.attributes(.Negative, draw.box(draw.label(
         .Left,
-        "'" ++ Key.toStr(help_key) ++ "': hide or show this message\n" ++
-            "'" ++ Key.toStr(quit_key) ++ "': quit\n" ++
-            "'" ++ Key.toStr(save_key) ++ "': save\n" ++
-            "'" ++ Key.toStr(undo_key) ++ "': undo\n" ++
-            "'" ++ Key.toStr(copy_key) ++ "': copy\n" ++
-            "'" ++ Key.toStr(paste_key) ++ "': paste\n" ++
-            "'" ++ Key.toStr(select_all_key) ++ "': select all\n" ++
-            "'" ++ Key.toStr(move_up_key) ++ "': move cursors up\n" ++
-            "'" ++ Key.toStr(move_down_key) ++ "': move cursors down\n" ++
-            "'" ++ Key.toStr(move_left_key) ++ "': move cursors left\n" ++
-            "'" ++ Key.toStr(move_right_key) ++ "': move cursors right\n" ++
-            "'" ++ Key.toStr(move_page_up_key) ++ "': move cursors up one screen\n" ++
-            "'" ++ Key.toStr(move_page_down_key) ++ "': move cursors down one screen\n" ++
-            "'" ++ Key.toStr(move_start_key) ++ "': move cursors to start of file\n" ++
-            "'" ++ Key.toStr(move_end_key) ++ "': move cursors to end of file\n" ++
-            "'" ++ Key.toStr(move_select_up_key) ++ "': move selections up\n" ++
-            "'" ++ Key.toStr(move_select_down_key) ++ "': move selections down\n" ++
-            "'" ++ Key.toStr(move_select_left_key) ++ "': move selections left\n" ++
-            "'" ++ Key.toStr(move_select_right_key) ++ "': move selections right\n" ++
-            "'" ++ Key.toStr(spawn_cursor_up_key) ++ "': spawn a cursor above the main cursor\n" ++
-            "'" ++ Key.toStr(spawn_cursor_down_key) ++ "': spawn a cursor below the main cursor\n" ++
-            "'" ++ Key.toStr(spawn_cursor_left_key) ++ "': spawn a cursor to the left of the main cursor\n" ++
-            "'" ++ Key.toStr(spawn_cursor_right_key) ++ "': spawn a cursor to the right of the main cursor\n" ++
-            "'" ++ Key.toStr(delete_left_key) ++ "': delete letter to the left of cursors\n" ++
-            "'" ++ Key.toStr(delete_right_key) ++ "': delete letter to the right of cursors\n" ++
-            "'" ++ Key.toStr(jump_key) ++ "': jump to line\n" ++
-            "'" ++ Key.toStr(reset_key) ++ "': general key to cancel/reset/stop the current action",
+        "'" ++ mem.toSliceConst(u8, &Key.toStr(quit_key, .NotCtrl)) ++ "': quit\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(save_key, .NotCtrl)) ++ "': save\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(undo_key, .NotCtrl)) ++ "': undo\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(copy_key, .NotCtrl)) ++ "': copy\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(paste_key, .NotCtrl)) ++ "': paste\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(select_all_key, .NotCtrl)) ++ "': select all\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(jump_key, .NotCtrl)) ++ "': jump to line\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(help_key, .CtrlAlphaNum)) ++ "': hide or show this message\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(reset_key, .NotCtrl)) ++ "': general key to cancel/reset/stop the current action\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(delete_left_key, .NotCtrl)) ++ "': delete letter to the left of cursors\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(delete_right_key, .NotCtrl)) ++ "': delete letter to the right of cursors\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_page_up_key, .NotCtrl)) ++ "': move cursors up one screen\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_page_down_key, .NotCtrl)) ++ "': move cursors down one screen\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_start_key, .NotCtrl)) ++ "': move cursors to start of file\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_end_key, .NotCtrl)) ++ "': move cursors to end of file\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_up_key, .NotCtrl)) ++ "': move cursors up\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_down_key, .NotCtrl)) ++ "': move cursors down\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_left_key, .NotCtrl)) ++ "': move cursors left\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_right_key, .NotCtrl)) ++ "': move cursors right\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_select_up_key, .NotCtrl)) ++ "': move selections up\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_select_down_key, .NotCtrl)) ++ "': move selections down\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_select_left_key, .NotCtrl)) ++ "': move selections left\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(move_select_right_key, .NotCtrl)) ++ "': move selections right\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(spawn_cursor_up_key, .NotCtrl)) ++ "': spawn a cursor above the main cursor\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(spawn_cursor_down_key, .NotCtrl)) ++ "': spawn a cursor below the main cursor\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(spawn_cursor_left_key, .NotCtrl)) ++ "': spawn a cursor to the left of the main cursor\n" ++
+            "'" ++ mem.toSliceConst(u8, &Key.toStr(spawn_cursor_right_key, .NotCtrl)) ++ "': spawn a cursor to the right of the main cursor",
     ))))));
 };
 
@@ -131,7 +134,7 @@ const quit_popup = blk: {
         .Center,
         "Warning!\n" ++
             "You have unsaved changes.\n" ++
-            "Press '" ++ Key.toStr(quit_key) ++ "' to forcefully quit",
+            "Press '" ++ mem.toSliceConst(u8, &Key.toStr(quit_key, .NotCtrl)) ++ "' to forcefully quit",
     ))))));
 };
 
@@ -194,6 +197,7 @@ pub fn main() !void {
         try stdout_buf.flush();
 
         const key = try input.readKey(stdin);
+        info.key = key;
         app = (try handleInput(app, key)) orelse break;
     }
 
@@ -207,35 +211,40 @@ const App = struct {
     view: @typeOf(window_view),
 };
 
-const help_key = Key.ctrl | Key.alt | 'h';
-const quit_key = Key.ctrl | 'q';
-const save_key = Key.ctrl | 's';
-const undo_key = Key.ctrl | 'z';
-const copy_key = Key.ctrl | 'c';
-const paste_key = Key.ctrl | 'v';
-const select_all_key = Key.ctrl | 'a';
-const move_up_key = Key.arrow_up;
-const move_down_key = Key.arrow_down;
-const move_left_key = Key.arrow_left;
-const move_right_key = Key.arrow_right;
+const reset_key = Key.escape;
+
+const help_key = Key.alt | Key.ctrl_h;
+const quit_key = Key.ctrl_q;
+const save_key = Key.ctrl_s;
+const undo_key = Key.ctrl_z;
+const copy_key = Key.ctrl_c;
+const paste_key = Key.ctrl_v;
+const select_all_key = Key.ctrl_a;
+const jump_key = Key.ctrl_j;
+
+const delete_left_key = Key.backspace;
+const delete_left2_key = Key.backspace2;
+const delete_right_key = Key.delete;
+
 const move_page_up_key = Key.page_up;
 const move_page_down_key = Key.page_down;
 const move_start_key = Key.home;
 const move_end_key = Key.end;
-const move_select_up_key = Key.shift | Key.arrow_up;
-const move_select_down_key = Key.shift | Key.arrow_down;
-const move_select_left_key = Key.shift | Key.arrow_left;
-const move_select_right_key = Key.shift | Key.arrow_right;
-const spawn_cursor_up_key = Key.ctrl | Key.arrow_up;
-const spawn_cursor_down_key = Key.ctrl | Key.arrow_down;
-const spawn_cursor_left_key = Key.ctrl | Key.arrow_left;
-const spawn_cursor_right_key = Key.ctrl | Key.arrow_right;
-const delete_left_key = Key.backspace;
-const delete_right_key = Key.delete;
-const jump_key = Key.ctrl + 'j';
-const reset_key = Key.esc;
 
-const default_hint = "'" ++ Key.toStr(help_key) ++ "' for help";
+const move_up_key = Key.arrow_up;
+const move_down_key = Key.arrow_down;
+const move_left_key = Key.arrow_left;
+const move_right_key = Key.arrow_right;
+const move_select_up_key = Key.shift_arrow_up;
+const move_select_down_key = Key.shift_arrow_down;
+const move_select_left_key = Key.shift_arrow_left;
+const move_select_right_key = Key.shift_arrow_right;
+const spawn_cursor_up_key = Key.ctrl_arrow_up;
+const spawn_cursor_down_key = Key.ctrl_arrow_down;
+const spawn_cursor_left_key = Key.ctrl_arrow_left;
+const spawn_cursor_right_key = Key.ctrl_arrow_right;
+
+const default_hint = "'" ++ mem.toSliceConst(u8, &Key.toStr(help_key, .CtrlAlphaNum)) ++ "' for help";
 
 fn handleInput(app: App, key: Key.Type) !?App {
     var editor = app.editor;
@@ -361,7 +370,7 @@ fn handleInput(app: App, key: Key.Type) !?App {
         spawn_cursor_right_key => text = try text.spawnCursor(.Right),
 
         // Delete
-        delete_left_key => text = try text.delete(.Left),
+        delete_left_key, delete_left2_key => text = try text.delete(.Left),
         delete_right_key => text = try text.delete(.Right),
 
         jump_key => view.children.editor.children.goto_prompt_bar.visibility = .Show,
